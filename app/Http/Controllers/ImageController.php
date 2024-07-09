919f215867086e012c5d32759ca9204b037552ea
gabrielciprianoo <devgabrielcipriano@gmail.com>
2024-07-09 17:04:06 -0600

    feat(ImageController): store function -resize images uploaded with intervention image -save image in the server

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class ImageController extends Controller
+{
+    //
+    public function store(Request $request){
+
+        $image = $request->file('file');
+
+        $imageName = Str::uuid() . "." . $image->extension();
+
+        //read and resize image with intervention image
+        $manager = new ImageManager(new Driver());
+        $serverImage = $manager->read($image);
+        $serverImage->resizeDown(1000,1000);
+
+        //store image in uploads folder
+
+        $imagePath = public_path('uploads') . '/' . $imageName;
+        $serverImage->save($imagePath);
+
+        return response()->json(['image' => $imageName]);
+    }
+}
