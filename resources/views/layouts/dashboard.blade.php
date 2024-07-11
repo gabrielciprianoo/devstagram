ec53fe79744156f932d33319052af413582054f2
gabrielciprianoo <devgabrielcipriano@gmail.com>
2024-07-10 19:52:57 -0600

    feat(posts section): section to show user post added

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -18,4 +18,23 @@
             </div>
         </div>
     </div>
+
+    <section class="container mx-auto m-20">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciónes</h2>
+        @if ($posts->count())
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                @foreach ($posts as $post)
+                    <div>
+                        <a href=" {{route('posts.show',['user' => $user, 'post' => $post])}}">
+                            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Imagen de post {{ $post->title }}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="mt-10">{{$posts->links()}}</div>
+        @else
+            <p class="text-xl text-center text-gray-600 my-20">No Hay Publicaciónes</p>
+        @endif
+    </section>
 @endsection
