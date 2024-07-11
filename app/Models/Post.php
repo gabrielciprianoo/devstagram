ac2e21bb9dca854a09cfd4590d5026ee4333d65c
gabrielciprianoo <devgabrielcipriano@gmail.com>
2024-07-10 19:49:01 -0600

    feat(Post): post belongs method added to relationship with users

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,9 @@ class Post extends Model
         'image',
         'user_id',
     ];
+
+    //define relationship with post owner user
+    public function user(){
+        return $this->belongsTo(User::class)->select(['name', 'username']);
+    }
 }
